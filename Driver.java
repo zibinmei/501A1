60189d4a67db42e6be955e784e314a8b0d70ced9
zibinmei <zibinmei@outlook.com>
2017-10-05 11:22:21 -0600

    Delete some comments

--- Driver.java
+++ Driver.java
@@ -1,20 +1,19 @@
 //Zibin Mei
 //1085722
-//Tut 01
-//Ver 5
+
 
 /* this program is a listed links managers
  * meanto storage movie coolections
- * Feature: 
- * -add item 
- * -remove item 
+ * Feature:
+ * -add item
+ * -remove item
  * -display all items
  * -search items in the list.
  * Limitation:
  * - only storage Movie datas
  * - no way to move the item around
  * - the list is empty, user have to input something
- * - remove only the last node 
+ * - remove only the last node
  */
 
 
@@ -23,16 +22,16 @@ import java.lang.Character;
 
 
 
-//main driver class 
-public class Driver		
-{		
+//main driver class
+public class Driver
+{
 	public static void main(String [] args)
     {
 		//initize the UI class
 		UserInterface a= new UserInterface();
 		a.start();
-		
-		
+
+
 	}
 
 
